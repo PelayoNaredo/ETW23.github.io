939ab1bc06fbf3d3d57775e84d847ae37f65e5ba
ETW23 <145110013+ETW23@users.noreply.github.com>
2023-09-15 13:05:54 +0200

    Add files via upload

---
+++ formacion/js/script.js
@@ -0,0 +1,25 @@
+///////////// MENU ///////////////////
+document.getElementById("menunav").style.right = "-20em";
+document
+  .getElementById("botonHamburguesa")
+  .addEventListener("click", function () {
+    var menu = document.getElementById("menunav");
+    if (menu.style.right == "-20em") {
+      menu.style.right = "0em";
+      menu.style.transition = "1s";
+    } else {
+      menu.style.right = "-20em";
+    }
+  });
+
+document.getElementById("modulos").style.height = "0px";
+document.getElementById("botonModus").onclick = function () {
+  var modus = document.getElementById("modulos");
+  if (modus.style.height == "0px") {
+    modus.style.height = "209px";
+    document.getElementById("botonModus").style.textDecoration = "underline";
+  } else {
+    modus.style.height = "0px";
+    document.getElementById("botonModus").style.textDecoration = "none";
+  }
+};
