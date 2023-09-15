a3c523f47c12cb51aba836cfa5eb4ec7ba652bbb
ETW23 <145110013+ETW23@users.noreply.github.com>
2023-09-15 14:24:29 +0200

    Equipo gabriel kevin
    awdad

---
+++ equipo/JS/index_script.js
@@ -0,0 +1,42 @@
+document.getElementById("menunav").style.right = "-20em";
+document
+  .getElementById("botonHamburguesa")
+  .addEventListener("click", function () {
+    var menu = document.getElementById("menunav");
+    if (menu.style.right == "-20em") {
+      menu.style.right = "0";
+      menu.style.top = "0.05em";
+      menu.style.transition = "1s";
+    } else {
+      menu.style.right = "-20em";
+    }
+  });
+
+document.getElementById("modulos").style.height = "0px";
+document
+  .getElementById("botonModus")
+  .onclick = function () {
+    var modus = document.getElementById("modulos")
+    if (modus.style.height == "0px") {
+      modus.style.height = "209px";
+      document.getElementById('botonModus').style.textDecoration = "underline";
+    } else {
+      modus.style.height = "0px";
+      document.getElementById('botonModus').style.textDecoration = "none";
+    }
+  };
+
+  const hambnav = document.getElementById("botonHamburguesa");
+  const menuMenu = document.getElementById("menu");
+  
+  let menuVisible = false;
+  
+  hambnav.addEventListener("click", () => {
+    if (menuVisible) {
+      menuMenu.style.display = "none";
+    } else {
+      menuMenu.style.display = "flex";
+    }
+    menuVisible = !menuVisible;
+  });
+  
